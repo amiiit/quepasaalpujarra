26a0351d92b1e2851fc5069504e51df67c774f8d
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2017-10-29 20:27:51 +0100

    Close popup with Escape

--- src/popup.js
+++ src/popup.js
@@ -7,19 +7,26 @@ export default class Popup {
     this.dom.wrapper = el
     this.dom.close = this.dom.wrapper.getElementsByClassName('close-button')[0]
     this.dom.content = this.dom.wrapper.getElementsByClassName('popup-content')[0]
-    this.dom.close.addEventListener('click', ()=>{
+    this.dom.close.addEventListener('click', () => {
       this.close()
     })
-
   }
 
-  open(contentId){
+  handleKeyDown = (e) => {
+    if (e.key === 'Escape') {
+      this.close()
+    }
+  }
+
+  open(contentId) {
     this.dom.wrapper.classList.add('opened')
     this.dom.content.innerHTML = `<h2>${contentId}</h2>`
+    document.body.addEventListener('keydown', this.handleKeyDown)
   }
 
-  close(){
+  close() {
     this.dom.wrapper.classList.remove('opened')
+    document.body.removeEventListener('keydown', this.handleKeyDown)
   }
 
 }
